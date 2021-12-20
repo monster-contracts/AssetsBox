cb76eca4ff7843295f4267c0e03616cc7bd89816
thlynn <hlxstc680@gmail.com>
2021-12-20 16:46:47 +0800

    publish to nmpjs

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -13,7 +13,7 @@ contract Whitelist {
     mapping(address => bool) public isApproved;
 
     address public immutable ms;
-    string public symbol;
+    string private _symbol;
 
     modifier is_approved() {
         require(isApproved[msg.sender], "Not approved");
@@ -24,24 +24,28 @@ contract Whitelist {
 
     constructor(address ms_, string memory symbol_) {
        ms = ms_;
-       symbol = symbol_;
+       _symbol = symbol_;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
     }
 
     function whitelist(uint proposalIndex) external {
-        string memory _symbol;
+        string memory returnedSymbol;
         uint approved = 0;
         address operator = address(0);
         bool arg = false;
         
-        (_symbol, approved, operator, arg) = IMultiSignature(ms).is_apporved(proposalIndex);
+        (returnedSymbol, approved, operator, arg) = IMultiSignature(ms).is_apporved(proposalIndex);
         
         require(!hasBeenProcessed[proposalIndex], "Proposal has been processed");
-        require(keccak256(abi.encodePacked(_symbol)) == keccak256(abi.encodePacked(symbol)));
+        require(keccak256(abi.encodePacked(returnedSymbol)) == keccak256(abi.encodePacked(symbol())));
         require(approved >= 2, "Less than 2");
 
         isApproved[operator] = arg;
         hasBeenProcessed[proposalIndex] = true;
 
-        emit Whitelisted(_symbol, proposalIndex, operator, arg);
+        emit Whitelisted(symbol(), proposalIndex, operator, arg);
     }
 }
